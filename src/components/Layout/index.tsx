8d17c1edd4f1ffefcf1208f3b2f614c84ee7f935
bhuynh <bhuynh@>
2021-09-11 14:47:27 -0700

    enhance ui via layout & navbar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -23,15 +23,11 @@ export const AppLayout = React.memo(({ children }) => {
                 <h2>Solana DAPP</h2>
               </div>
             </Link>
-            <AppBar />
             <Navbar />
           </Header>
           <div className="relative md:ml-64 bg-blueGray-100">
-          <div className="relative bg-blue-600 md:pt-32 pb-32 pt-12">
+          <div className="relative bg-blue-600 md:pt-8 pb-24 pt-12">
           <div className="px-4 md:px-10 mx-auto w-full">
-            <div>
-              {/* Card stats */}
-            </div>
           </div>
         </div>
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
@@ -40,7 +36,7 @@ export const AppLayout = React.memo(({ children }) => {
           <div className="flex flex-wrap mt-4">
             <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-                <div className="block w-full overflow-x-auto">
+                <div className="block w-full overflow-x-auto px-4">
                   {/* All Paged Components */}
                   {children}    
                 </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,21 @@
+import { SettingOutlined } from "@ant-design/icons";
+import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-ant-design";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Button, Popover } from "antd";
 import React from "react";
+import { Settings } from "../Settings";
+import { LABELS } from "../../constants";
+
 
 // import UserDropdown from "./UserDropdown.js";
 
 export default function Navbar() {
+  const { connected } = useWallet();
+
   return (
     <>
       {/* Navbar */}
-      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-1">
         <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Brand */}
           <a
@@ -17,14 +26,25 @@ export default function Navbar() {
             Dashboard
           </a>
           {/* Form */}
-          <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-            <div className="relative flex w-full flex-wrap items-stretch">
-              <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i className="fas fa-search"></i>
-              </span>
-              <input type="text" placeholder="Search here..." className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+          <div className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto">
+            <WalletMultiButton type="primary" />
+          </div>
+          <div className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto">
+          {connected ? <WalletDisconnectButton type="text" /> : null}
+            <Popover
+              placement="topRight"
+              title={LABELS.SETTINGS_TOOLTIP}
+              content={<Settings />}
+              trigger="click"
+              >
+            <Button
+              shape="circle"
+              size="large"
+              type="text"
+              icon={<SettingOutlined />}
+            />
+            </Popover>
             </div>
-          </form>
           {/* User */}
           <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
             {/* <UserDropdown /> */}
